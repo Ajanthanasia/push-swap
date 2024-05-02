37b5cdd7c62654bd3afb444264835fe7ff8dc1f6
Ajanthan <ajanthanasia@gmail.com>
2024-05-02 22:28:25 +0530

    done

--- Bonus/print_array.php
+++ Bonus/print_array.php
@@ -2,7 +2,7 @@
 function printArray($name, $arr)
 {
     echo $name;
-    echo " : ";
+    echo " ";
     $length = count($arr);
     for ($i = 0; $i < $length; $i++) {
         echo $arr[$i];
--- Bonus/push_swap.php
+++ Bonus/push_swap.php
@@ -4,16 +4,20 @@ include_once "command_her.php";
 $there = array();
 $lb = array();
 
-$no = readline("Enter array there elements : ");
-$num_array = explode(" ", $no);
-$there = array_map('intval', $num_array);
-
-sort($there);
-printArray('there', $there);
-printArray('lb', $lb);
-$comm = readline("Your command : ");
-if ($comm == 'her' || $comm == 'Her' || $comm == 'HER') {
-    $there = her($there);
-    printArray('there', $there);
-    printArray('lb', $lb);
+$there = array();
+$lb = array();
+$len = count($argv);
+for ($i = 1; $i < $len; $i++) {
+    $n = intval($argv[$i]);
+    if ((is_int($n)) && ($n > 0)) {
+        array_push($there, $n);
+    } else {
+        sort($there);
+        $comm = $argv[$i];
+        if ($comm == 'her' || $comm == 'Her' || $comm == 'HER') {
+            $there = her($there);
+            printArray('there', $there);
+            printArray('lb', $lb);
+        }
+    }
 }
