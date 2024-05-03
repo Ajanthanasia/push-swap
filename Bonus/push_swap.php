e84979b794114ce48a21bf3bf771841901fab5f0
ajanthan-hippop <ajanthan.dev@hotelshippo.lk>
2024-05-03 15:18:08 +0530

    done

--- Bonus/push_swap.php
+++ Bonus/push_swap.php
@@ -14,42 +14,28 @@ for ($i = 1; $i < $len; $i++) {
         $comm = $argv[$i];
         if ($comm == 'her' || $comm == 'Her' || $comm == 'HER') {
             $there = her($there);
-            printArray('there', $there);
-            printArray('lb', $lb);
         } else if ($comm == 'sb' || $comm == 'Sb' || $comm == 'SB') {
             $lb = sb($lb);
-            printArray('there', $there);
-            printArray('lb', $lb);
         } else if ($comm == 'sc') {
             $there = her($there);
             $lb = sb($lb);
-            printArray('there', $there);
-            printArray('lb', $lb);
         } else if ($comm == 'pa') {
             $arrs = pa($there, $lb);
             $there = $arrs[0];
             $lb = $arrs[1];
-            printArray('there', $there);
-            printArray('lb', $lb);
         } else if ($comm == 'pb') {
             $arrs = pb($there, $lb);
             $there = $arrs[0];
             $lb = $arrs[1];
-            printArray('there', $there);
-            printArray('lb', $lb);
         } else if ($comm == 'ra') {
             $there = ra($there);
-            printArray('there', $there);
-            printArray('lb', $lb);
         } else if ($comm == 'rb') {
             $lb = rb($lb);
-            printArray('there', $there);
-            printArray('lb', $lb);
         } else if ($comm == 'rr') {
             $there = ra($there);
             $lb = rb($lb);
-            printArray('there', $there);
-            printArray('lb', $lb);
         }
     }
 }
+printArray('there', $there);
+printArray('lb', $lb);
