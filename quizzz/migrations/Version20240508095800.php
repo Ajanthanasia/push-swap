969f9e5c3501f4c26834489c37647a2ee2d46b4a
ajanthan-hippop <ajanthan.dev@hotelshippo.lk>
2024-05-08 16:13:39 +0530

    register user

--- quizzz/migrations/Version20240508095800.php
+++ quizzz/migrations/Version20240508095800.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240508093130 extends AbstractMigration
+final class Version20240508095800 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- quizzz/src/Controller/HomeController.php
+++ quizzz/src/Controller/HomeController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\RegisterUserFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
@@ -26,4 +31,29 @@ class HomeController extends AbstractController
         ]);
     }
 
+    #[Route('/store', name: 'store-user')]
+    public function store(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hash)
+    {
+        $form = $this->createForm(RegisterUserFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userData = $form->getData();
+
+            $user = new User();
+            $user->setEmail($userData['email']);
+            $user->setRoles(['action' => 'nothing']);
+            $password = $hash->hashPassword($user, $userData['password']);
+            $user->setPassword($password);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Optionally, add a flash message or redirect to another page
+            $this->addFlash('success', 'User created successfully.');
+
+            // Redirect to a success page
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->redirectToRoute('app_home');
+    }
 }
