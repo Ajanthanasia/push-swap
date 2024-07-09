f5b6b546d2ccec65d2b5c3c932f43cd1c518dd3c
ajanthan-hippop <ajanthan.dev@hotelshippo.lk>
2024-07-09 13:29:39 +0530

    done

--- eval/tester.php
+++ eval/tester.php
@@ -1,35 +1,37 @@
 <?php
-function eval_expr($expr)
-{
-    if (is_numeric($expr)) {
-        $intValue = intVal($expr);
-        return $intValue;
-    } else {
-        $i = 0;
-        $num = "";
-        while (is_numeric($expr[$i])) {
-            $num .= $expr[$i];
+$file = $argv[1];
+$handle = fopen($file, "r");
+if ($handle) {
+    function eval_expr($expr)
+    {
+        if (is_numeric($expr)) {
+            $intValue = intVal($expr);
+            return $intValue;
+        } else {
+            $i = 0;
+            $num = "";
+            while (is_numeric($expr[$i])) {
+                $num .= $expr[$i];
+                $i++;
+            }
+            $intNum = intVal($num);
+            $operator = $expr[$i];
             $i++;
-        }
-        $intNum = intVal($num);
-        $operator = $expr[$i];
-        $i++;
-        $restOfString = substr($expr, $i, strlen($expr));
-        if ($operator == "+") {
-            return $intNum + eval_expr($restOfString);
-        } else if ($operator == "-") {
-            return $intNum - eval_expr($restOfString);
-        } else if ($operator == "*") {
-            return $intNum * eval_expr($restOfString);
-        } else if ($operator == "/") {
-            return $intNum / eval_expr($restOfString);
-        } else if ($operator == "%") {
-            return $intNum % eval_expr($restOfString);
+            $restOfString = substr($expr, $i, strlen($expr));
+            if ($operator == "+") {
+                return $intNum + eval_expr($restOfString);
+            } else if ($operator == "-") {
+                return $intNum - eval_expr($restOfString);
+            } else if ($operator == "*") {
+                return $intNum * eval_expr($restOfString);
+            } else if ($operator == "/") {
+                return $intNum / eval_expr($restOfString);
+            } else if ($operator == "%") {
+                return $intNum % eval_expr($restOfString);
+            }
         }
     }
+    $expr = fgets($handle);
+    print_r(eval_expr($expr));
+    fclose($handle);
 }
-
-$expr = $argv[1];
-
-// echo eval_expr($expr);
-// echo handle_input(eval_expr($expt));
