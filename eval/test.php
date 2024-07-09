ade10e1aea8bbfabb52f421debd5b4d575deba3d
ajanthan-hippop <ajanthan.dev@hotelshippo.lk>
2024-07-09 11:20:17 +0530

    running

--- eval/test.php
+++ eval/test.php
@@ -1,54 +1,34 @@
 <?php
-function evaluate_expression($expr) {
-
-  
-    // Supported operators with their corresponding functions
-    $operators = [
-      '+' => function($a, $b) { return $a + $b; },
-      '-' => function($a, $b) { return $a - $b; },
-      '*' => function($a, $b) { return $a * $b; },
-      '/' => function($a, $b) {
-        if ($b === 0) {
-          throw new InvalidArgumentException("Division by zero");
-        }
-        return $a / $b;
-      },
-    ];
-  
-    $stack = [];
-  
-    try {
-      // Split the expression with basic whitespace and handle extra spaces
-      $tokens = preg_split('/\s+/', trim($expr), -1, PREG_SPLIT_NO_EMPTY);
-  
-      foreach ($tokens as $token) {
-        if (isset($operators[$token])) {
-          $operator = $operators[$token];
-          $operand2 = array_pop($stack);
-          $operand1 = array_pop($stack);
-          $result = $operator($operand1, $operand2);
-          array_push($stack, $result);
-        } else {
-          if (!is_numeric($token)) {
-            throw new InvalidArgumentException("Invalid characters in expression: $token");
-          }
-          array_push($stack, (float) $token);
-        }
-      }
-  
-      if (count($stack) !== 1) {
-        throw new InvalidArgumentException("Invalid expression syntax");
-      }
-  
-      return array_pop($stack);
-    } catch (Exception $e) {
-      throw new InvalidArgumentException("Error evaluating expression: " . $e->getMessage());
+function evaluate_expression($expr)
+{
+  if (is_numeric($expr)) {
+    $intValue = intVal($expr);
+    return $intValue;
+  } else {
+    $i = 0;
+    $num = "";
+    while (is_numeric($expr[$i])) {
+      $num .= $expr[$i];
+      $i++;
+    }
+    $intNum = intVal($num);
+    $operator = $expr[$i];
+    $i++;
+    $restOfString = substr($expr, $i, strlen($expr));
+    if ($operator == "+") {
+      return $intNum + evaluate_expression($restOfString);
+    } else if ($operator == "-") {
+      return $intNum - evaluate_expression($restOfString);
+    } else if ($operator == "*") {
+      return $intNum * evaluate_expression($restOfString);
+    } else if ($operator == "/") {
+      return $intNum / evaluate_expression($restOfString);
     }
   }
-  
-  // Example usage
-  try {
-    echo evaluate_expression("2 + 9") . PHP_EOL;  // Throws InvalidArgumentException
-  } catch (InvalidArgumentException $e) {
-    echo "Error: " . $e->getMessage() . PHP_EOL;
-  }
+}
+// Example usage
+// $result = evaluate_expression("1+21");
+// $result = evaluate_expression("12-3");
+// $result = evaluate_expression("12*12");
+$result = evaluate_expression("12/2");
+echo $result;
