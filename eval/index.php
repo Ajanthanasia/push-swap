f8175cfee4348eacdb3cad18337d7947704d60da
ajanthan-hippop <ajanthan.dev@hotelshippo.lk>
2024-07-09 15:32:05 +0530

    bodmas

--- eval/index.php
+++ eval/index.php
@@ -1,31 +1,98 @@
 <?php
+
 function eval_expr(string $expr)
 {
-    if (is_numeric($expr)) {
-        $intValue = intVal($expr);
-        return $intValue;
-    } else {
+    // Helper function to evaluate basic binary expressions
+    function eval_basic_expr($num1, $operator, $num2)
+    {
+        switch ($operator) {
+            case '+':
+                return $num1 + $num2;
+            case '-':
+                return $num1 - $num2;
+            case '*':
+                return $num1 * $num2;
+            case '/':
+                return $num1 / $num2;
+            case '%':
+                return $num1 % $num2;
+        }
+        return 0; // Default case, should never be reached
+    }
+
+    // Function to handle precedence using the Shunting Yard algorithm
+    function parse_to_rpn($expr)
+    {
+        $output = [];
+        $operators = [];
         $i = 0;
-        $num = "";
-        while (is_numeric($expr[$i])) {
-            $num .= $expr[$i];
+        $length = strlen($expr);
+        while ($i < $length) {
+            $char = $expr[$i];
+
+            // If it's a number, parse the full number
+            if (is_numeric($char)) {
+                $num = '';
+                while ($i < $length && is_numeric($expr[$i])) {
+                    $num .= $expr[$i];
+                    $i++;
+                }
+                $output[] = $num;
+                continue;
+            }
+
+            // If it's an operator, handle according to precedence
+            if (in_array($char, ['+', '-', '*', '/', '%'])) {
+                while (!empty($operators) && precedence(end($operators)) >= precedence($char)) {
+                    $output[] = array_pop($operators);
+                }
+                $operators[] = $char;
+            }
+
             $i++;
         }
-        $intNum = intVal($num);
-        $operator = $expr[$i];
-        $i++;
-        $restOfString = substr($expr, $i, strlen($expr));
-        if ($operator == "+") {
-            return $intNum + eval_expr($restOfString);
-        } else if ($operator == "-") {
-            return $intNum - eval_expr($restOfString);
-        } else if ($operator == "*") {
-            return $intNum * eval_expr($restOfString);
-        } else if ($operator == "/") {
-            return $intNum / eval_expr($restOfString);
-        } else if ($operator == "%") {
-            return $intNum % eval_expr($restOfString);
+
+        // Pop all remaining operators to output
+        while (!empty($operators)) {
+            $output[] = array_pop($operators);
         }
+
+        return $output;
     }
+
+    // Function to evaluate RPN expression
+    function eval_rpn($rpn)
+    {
+        $stack = [];
+        foreach ($rpn as $token) {
+            if (is_numeric($token)) {
+                $stack[] = $token;
+            } else {
+                $num2 = array_pop($stack);
+                $num1 = array_pop($stack);
+                $stack[] = eval_basic_expr($num1, $token, $num2);
+            }
+        }
+        return array_pop($stack);
+    }
+
+    // Helper function to define operator precedence
+    function precedence($operator)
+    {
+        switch ($operator) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+            case '%':
+                return 2;
+        }
+        return 0;
+    }
+
+    // Convert expression to Reverse Polish Notation (RPN)
+    $rpn = parse_to_rpn($expr);
+    // Evaluate the RPN expression
+    return eval_rpn($rpn);
 }
-?>
