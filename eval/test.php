db3418671dfa8df5e7160e91d46e197afc4a24ea
ajanthan-hippop <ajanthan.dev@hotelshippo.lk>
2024-07-09 13:49:04 +0530

    done

--- eval/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Example usage
-// $result = eval_expr("1+21");
-// $result = eval_expr("12+3-6/3");
-// $result = eval_expr("19%50");
-// $result = eval_expr("50%6");
-require('./index.php');
-$expr = eval_expr("12/2+3");
-echo $expr;
