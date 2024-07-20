8da2e992012e5829a8b6402f7818a16d0e4f26c9
Ajanthan <ajanthanasia@gmail.com>
2024-07-20 17:15:47 +0530

    back

---
+++ backend/src/Controller/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+// use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('api/login-user', name: 'api_login_user', methods: ['post'])]
+    public function login(Request $request, UserRepository $userRepository): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $email = $data['email'] ?? null;
+        $password = $data['password'] ?? null;
+        if (!$email || !$password) {
+            return new JsonResponse(['TypeDanger' => 'Invalid credentials'], JsonResponse::HTTP_BAD_REQUEST);
+        } else {
+            $user = $userRepository->findOneUser($email);
+            // $user = $this->userProvider->loadUserByIdentifier($email);
+
+            if ($user == null) {
+                return new JsonResponse(['TypeDanger' => 'User does not exists'], JsonResponse::HTTP_BAD_REQUEST);
+            } else {
+                if (password_verify($password, $user->getPassword())) {
+                    return $this->json([
+                        'status' => true,
+                        'message' => 'Logined Successfully!',
+                        'user_id' => $user->getId(),
+                    ]);
+                } else {
+                    return new JsonResponse(['TypeDanger' => 'User Passwords does not correct'], JsonResponse::HTTP_BAD_REQUEST);
+                }
+            }
+        }
+
+        // return $this->json($data['email']);
+    }
+}
---
+++ backend/src/Security/AppAuthenticator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+
+class AppAuthenticator extends AbstractLoginFormAuthenticator
+{
+    public const LOGIN_ROUTE = 'api_login';
+
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($password)
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // Handle successful authentication
+        return new Response('Authenticated successfully', Response::HTTP_OK);
+    }
+
+    // public function onAuthenticationFailure(Request $request, \Exception $exception): ?Response
+    // {
+    //     // // Handle failed authentication
+    //     return new Response('Authentication failed', Response::HTTP_UNAUTHORIZED);
+    // }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
